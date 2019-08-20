ccf22a40b6d9bc1c57a5d55604cd58d67b921a02
ilyaCoolDev <ilya@d3en.ru>
2019-08-20 16:29:31 +0600

    added fourth loto

--- mathPuzzle/urls.py
+++ mathPuzzle/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('lvl1/', views.game, name='lvl1'),
     path('lvl2/', views.game, name='lvl2'),
     path('lvl3/', views.game, name='lvl3'),
+    path('lvl4/', views.game, name='lvl4'),
     path('instruction', views.instruction, name='instruction')
 ]
