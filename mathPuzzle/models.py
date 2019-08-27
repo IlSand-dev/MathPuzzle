d2158ef1680c50e642e6c768fc7ee64ad409e0a9
ilyaCoolDev <ilya@d3en.ru>
2019-08-27 21:10:32 +0600

    updated first level

--- mathPuzzle/models.py
+++ mathPuzzle/models.py
@@ -1,1 +1,13 @@
+import datetime
+from django.db import models
 
+
+class Question(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Question")
+    date_published = models.DateTimeField(verbose_name="Published_Date", default=datetime.datetime.now())
+
+
+class Answer(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=200)
+    is_right = models.BooleanField()
--- math_puzzle/urls.py
+++ math_puzzle/urls.py
@@ -16,19 +16,21 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+                  path('admin/', admin.site.urls),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
-    path(r'accounts/logout/', include('mathPuzzle.urls'))
-]
+                   path(r'accounts/logout/', include('mathPuzzle.urls'))
+               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-]
+                   url(r'^accounts/', include('django.contrib.auth.urls')),
+               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
-    path('', include('mathPuzzle.urls'))
-]
+                   path('', include('mathPuzzle.urls'))
+               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
